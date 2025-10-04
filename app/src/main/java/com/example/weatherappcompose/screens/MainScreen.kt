a67ea9eb30c62de372df2cf27e60c102975125e8
Evgen1994-McD <mrmarwell@gmail.com>
2025-10-04 17:37:48 +0500

    +LazyColumn +ListItem

--- app/src/main/java/com/example/weatherappcompose/screens/MainScreen.kt
+++ app/src/main/java/com/example/weatherappcompose/screens/MainScreen.kt
@@ -3,9 +3,11 @@ package com.example.weatherappcompose.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -201,6 +203,11 @@ fun TabLayout() {
             state = pagerState,
             modifier = Modifier.weight(1.0f)
         ) { index ->
+            LazyColumn(modifier = Modifier.fillMaxSize()){
+                items(count = 15){
+                    ListItem()
+                }
+            }
 
 
 
