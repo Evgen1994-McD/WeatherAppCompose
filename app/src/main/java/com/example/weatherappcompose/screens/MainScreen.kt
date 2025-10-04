4891fd37b093c2c265a6e179a443d6de4e5d2db0
Evgen1994-McD <mrmarwell@gmail.com>
2025-10-04 14:19:41 +0500

    + кард с основной инфой о погоде +иконки

--- app/src/main/java/com/example/weatherappcompose/screens/MainScreen.kt
+++ app/src/main/java/com/example/weatherappcompose/screens/MainScreen.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -55,21 +57,93 @@ fun MainScreen() {
             colors = CardDefaults.cardColors(containerColor = BlueLight)
         ) {
 
-            Column(modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally) {
-                Row(modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween){
-                    Text(modifier = Modifier.padding(top = 8.dp, start = 8.dp),
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        modifier = Modifier.padding(top = 8.dp, start = 8.dp),
                         text = "20 Jun 2025 13:00",
-                        style = TextStyle(fontSize = 15.sp,
-                            color = Color.White))
-                    AsyncImage(model = "https://cdn.weatherapi.com/weather/64x64/day/176.png",
+                        style = TextStyle(
+                            fontSize = 15.sp,
+                            color = Color.White
+                        )
+                    )
+                    AsyncImage(
+                        model = "https://cdn.weatherapi.com/weather/64x64/day/176.png",
                         contentDescription = "im2",
                         modifier = Modifier
                             .size(35.dp)
-                            .padding(top = 3.dp, end = 8.dp))
+                            .padding(top = 3.dp, end = 8.dp)
+                    )
 
                 }
+
+                Text(
+                    text = "Perm",
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        color = Color.White
+                    )
+                )
+
+                Text(
+                    text = "23°С",
+                    style = TextStyle(
+                        fontSize = 65.sp,
+                        color = Color.White
+                    )
+                )
+
+                Text(
+                    text = "Sunny",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        color = Color.White
+                    )
+                )
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(
+                        modifier = Modifier
+                            .size(32.dp)
+                            .padding(6.dp), onClick = {
+
+                        }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            contentDescription = "im3",
+                            tint = Color.White
+
+                        )
+                    }
+
+                    Text(modifier = Modifier.padding(top = 6.dp),
+                        text = "23°C/12°C",
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            color = Color.White
+                        )
+                    )
+                    IconButton(modifier = Modifier.size(32.dp), onClick = {
+
+                    }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.refresh),
+                            contentDescription = "im4",
+                            tint = Color.White
+                        )
+                    }
+
+                }
+
 
             }
         }
