63f66f93bd43fc8132b119258560ce8a75e9587a
Evgen1994-McD <mrmarwell@gmail.com>
2025-10-06 21:04:59 +0500

    19LAlert9.29

--- app/src/main/java/com/example/weatherappcompose/screens/UI.kt
+++ app/src/main/java/com/example/weatherappcompose/screens/UI.kt
@@ -11,6 +11,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.TextButton
+import androidx.compose.material.TextField
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -93,4 +96,45 @@ fun ListItem(item: WeatherModel, currentDay: MutableState<WeatherModel>) {
 
 
     }
+
 }
+
+
+@Composable
+fun DialogSearch(){
+    AlertDialog(onDismissRequest = {
+
+    },
+        confirmButton = {
+            TextButton(onClick = {
+
+            }) {
+                Text(text = "OK")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = {
+
+            }) {
+
+                Text(text = "Cancel")
+            }
+        },
+
+        title = {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Text(text = "Введите название города:")
+                TextField(value = "Text", onValueChange = {
+
+                })
+
+            }
+        }
+
+
+
+
+        )
+}
+
+
